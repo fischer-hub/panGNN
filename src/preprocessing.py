fix underflow resulting in 0 division and nan values

src/preprocessing.py
@@ -458,56 +458,46 @@ def load_similarity_score(similarity_score_file):
 
 
 
-def normalize_sim_scores(sim_score_dict, t = 1, epsilon=1e-8):
-
+def normalize_sim_scores(sim_score_dict, t = 1):
+    
     normalized_dict = {}
 
     for origin_gene in sim_score_dict.keys():
 
-        sub_dict_lst = []
-        genome_ids_seen = []
-        sub_dict = {}
-        denominator = 0
-        candidate_dict = sim_score_dict[origin_gene]
+        candidate_genome_ids = set([id.split('_')[0] for id in sim_score_dict[origin_gene].keys()])
+        dict_lst = []
+        origin_gene_dict = {}
 
-        current_genome_ids = set([id.split('_')[0] for id in candidate_dict.keys()])
+        for candidate_genome_id in candidate_genome_ids:
 
-        # seleect one genome to observe candidates from
-        for current_genome_id in current_genome_ids:
-
-            # if we already seen this genome id, skip interation
-            if current_genome_id in genome_ids_seen:
-                continue
-
-            # save genome id so we dont check it twice later
-            genome_ids_seen.append(current_genome_id)
-
-            # write candidates to dict and calculate their probability denominator
-            for target_gene, sim_score in zip(candidate_dict.keys(), candidate_dict.values()):
-
-                # if gene is from genome with current genome id and not from the origin genome add to dict
-                if current_genome_id in target_gene:# and current_genome_id not in origin_gene:
-                    try:
-
-                        sub_dict.update({target_gene: sim_score})
-                        denominator += np.exp(-t * sim_score)
-                    except Exception as e:
-                        print(f"{sim_score}, {denominator}, {e}")
-
-            # divide every exponentiated score by the sum of the e. scores of the other candidates (plus epsilon to avoid division by zero)                
-            for gene_id, sim_score in zip(sub_dict.keys(), sub_dict.values()):
-                sub_dict[gene_id] = np.exp(-t * sim_score) / (denominator)
-
-
-            
-            sub_dict_lst.append(sub_dict)
+            genome_pair_dict = {}
             denominator = 0
+
+            for candidate_id, score in sim_score_dict[origin_gene].items():
+                
+                if candidate_genome_id in candidate_id:
+                    # this might cause an underflow in np.exp depending on how 
+                    # high the score is and how big t is which results in nan in 
+                    # the division if there are no other scores adding to the sum
+                    # in which case the normalized score will be set to 1
+                    exp_score = np.exp(-t * score)
+                    denominator += exp_score
+                    genome_pair_dict.update({candidate_id: exp_score})
+            
+            # all candidates with the current genome id are in the genome_pair_dict
+            
+            for candidate_id, exp_score in genome_pair_dict.items():
+                normalized_sim_score = exp_score / denominator
+                genome_pair_dict[candidate_id] = normalized_sim_score if not np.isnan(normalized_sim_score) else 1
+            
+            # all scores from the current genome are normalized
+            dict_lst.append(genome_pair_dict)
         
-        sub_dict = {}
-        for mydict in sub_dict_lst:
-            sub_dict.update(mydict)
-        
-        normalized_dict[origin_gene] = sub_dict
+        # all genomes scores are normalized for this origin gene, merge the dicts
+        for d in dict_lst:
+            origin_gene_dict.update(d)
+
+        normalized_dict[origin_gene] = origin_gene_dict
 
     # sanity check, is are all genes still in the dict, are all scores in range [0,1]?
     for gene_id in sim_score_dict.keys():