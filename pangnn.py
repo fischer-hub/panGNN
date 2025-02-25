fix nan infection during backpropagation on normalized sims cores

pangnn.py
@@ -16,14 +16,8 @@ from src.helper import generate_minimal_dataset, simulate_dataset, sub_sample_gr
 from sklearn.metrics import roc_curve, auc
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-###################
-### ENTRY POINT ###
-###################
-
-
 
 # low embedding dim will reduce risk of overfitting but may prevent model form learning nuanced patterns
-# we later concat the embeddings of each gene ID (node) with its neighbouring gene ID embedding -> 3 * embedding dim
 gene_id_embedding_dim = 64
 
 # more dims
@@ -32,22 +26,17 @@ hidden_dim = 64
 # this (the edge features) later holds the similarity bit score of MMSeqs2 clustering for each two gene nodes connected by an edge
 edge_feature_dim = 128
 
-#batch = torch.zeros(num_genes, dtype=torch.long)  # Batch vector for mini-batches if needed
-
-#simuoated_dataset = generate_data(20000, 5, 5, 0.5, 0.5, 0.02)
 
 #dataset = HomogenousDataset(args.annotation, args.similarity, args.ribap_groups, args.neighbours) if args.train else HomogenousDataset(args.annotation, args.similarity, args.neighbours)
 if not args.simulate_dataset:
     log.info('Simulating dataset.')
     num_genomes = len(args.annotation)
-    dataset = UnionGraphDataset(args.annotation, args.similarity, args.ribap_groups, args.neighbours, split=(0.8, 0.2), categorical_nodes = args.categorical_node) if args.train else HomogenousDataset(args.annotation, args.similarity, args.neighbours)
+    dataset = UnionGraphDataset(args.annotation, args.similarity, args.ribap_groups, args.neighbours, split=(0.6, 0.4), categorical_nodes = args.categorical_node) if args.train else HomogenousDataset(args.annotation, args.similarity, args.neighbours)
     #dataset.generate_graph_data()
 else:
     num_genomes = 3
     dataset  = UnionGraphDataset()
     dataset.simulate_dataset(2000, num_genomes, 0.15)
-
-
 
 
 #print(dataset.train.edge_attr.max())
@@ -141,7 +130,7 @@ elif args.train:
                 labels = batch[0].y if isinstance(batch, tuple) else batch.y
                 class_balance_factor = (labels == 0.).sum()/labels.sum()
                 criterion = torch.nn.BCEWithLogitsLoss(pos_weight = class_balance_factor)
-                print(class_balance_factor)
+                #print(class_balance_factor)
 
                 # clear old gradients so only current batch gradients are used
                 optimizer.zero_grad()
@@ -149,6 +138,7 @@ elif args.train:
                 # this calls the models forward function since model is callable
                 log.debug('Calling forward step on current batch..')
                 output = model(batch)
+                #print(f"logits after decoding:\n{output}")
                 log.debug('Calling loss function on current batch..')
                 log.debug(batch)
                 loss = criterion(output, labels)
@@ -193,7 +183,7 @@ elif args.train:
                 binary_prediction_val = binary_prediction_val.cpu()
                 binary_prediction = binary_prediction.cpu()
                 labels = labels.cpu()
-                
+                #print(f"logits: \n{output}")
                 conf_matrix = confusion_matrix(test_labels, binary_prediction_val)
                 tn, fp, fn, tp = conf_matrix.ravel()
 

src/gnn.py
@@ -117,9 +117,11 @@ class AlternateGCN(torch.nn.Module):
     def forward(self, graph):
     
         log.debug('Passing nodes to embedding layer..')
+        #assert not torch.isnan().any()
         log.debug(graph.x.shape)
         node_embeddings = self.embedding(graph.x)
         log.debug('Passing similarity graph data to convolution layer 1..')
+        torch.set_printoptions(threshold=1_000)
         # convolute over similarity edges
         nodes = self.conv_in(node_embeddings, graph.edge_index, graph.edge_attr)
         #nodes = self.activation_fct(nodes)
@@ -130,11 +132,11 @@ class AlternateGCN(torch.nn.Module):
         nodes = self.conv_hidden(nodes, graph.edge_index, graph.edge_attr)
         nodes = self.conv_hidden(nodes, graph.union_edge_index)
 
-        nodes = self.conv_hidden(nodes, graph.edge_index, graph.edge_attr)
+        """nodes = self.conv_hidden(nodes, graph.edge_index, graph.edge_attr)
         nodes = self.conv_hidden(nodes, graph.union_edge_index)
         
         nodes = self.conv_hidden(nodes, graph.edge_index, graph.edge_attr)
-        nodes = self.conv_hidden(nodes, graph.union_edge_index)
+        nodes = self.conv_hidden(nodes, graph.union_edge_index) """
 
         nodes = self.conv_hidden(nodes, graph.edge_index, graph.edge_attr)
         nodes = self.conv_out(nodes, graph.union_edge_index)
@@ -142,9 +144,9 @@ class AlternateGCN(torch.nn.Module):
 
         log.debug(f"Outputting nodes to decode function of shape: {nodes.shape}\n{nodes}")
 
-        link_predictions = self.decode(nodes, graph.edge_index)
-        #concat_node_embeddings = torch.cat((nodes[graph.edge_index[0]], nodes[graph.edge_index[1]]), dim = 1)
-        #link_predictions = self.mlp(concat_node_embeddings).squeeze(-1)
+        #link_predictions = self.decode(nodes, graph.edge_index)
+        concat_node_embeddings = torch.cat((nodes[graph.edge_index[0]], nodes[graph.edge_index[1]]), dim = 1)
+        link_predictions = self.mlp(concat_node_embeddings).squeeze(-1)
         #print(link_predictions)
 
         if not True:

src/preprocessing.py
@@ -458,9 +458,10 @@ def load_similarity_score(similarity_score_file):
 
 
 
-def normalize_sim_scores(sim_score_dict, t = 1):
+def normalize_sim_scores(sim_score_dict, t = 0.5, epsilon = 1e-8):
     
     normalized_dict = {}
+    empty_dict_ids = []
 
     for origin_gene in sim_score_dict.keys():
 
@@ -475,8 +476,8 @@ def normalize_sim_scores(sim_score_dict, t = 1):
 
             for candidate_id, score in sim_score_dict[origin_gene].items():
                 
-                if candidate_genome_id in candidate_id:
-                    # this might cause an underflow in np.exp depending on how 
+                if candidate_genome_id in candidate_id and candidate_id not in origin_gene:
+                    # this might cause an underflow in np.exp depending on how
                     # high the score is and how big t is which results in nan in 
                     # the division if there are no other scores adding to the sum
                     # in which case the normalized score will be set to 1
@@ -485,9 +486,9 @@ def normalize_sim_scores(sim_score_dict, t = 1):
                     genome_pair_dict.update({candidate_id: exp_score})
             
             # all candidates with the current genome id are in the genome_pair_dict
-            
+            # add epsilon for numeric stability            
             for candidate_id, exp_score in genome_pair_dict.items():
-                normalized_sim_score = exp_score / denominator
+                normalized_sim_score = (exp_score / denominator) + epsilon
                 genome_pair_dict[candidate_id] = normalized_sim_score if not np.isnan(normalized_sim_score) else 1
             
             # all scores from the current genome are normalized
@@ -497,11 +498,18 @@ def normalize_sim_scores(sim_score_dict, t = 1):
         for d in dict_lst:
             origin_gene_dict.update(d)
 
-        normalized_dict[origin_gene] = origin_gene_dict
+        # if dict is empty, skip this gene (e.g. it only holds sim scores to itself)
+        if origin_gene_dict:
+            normalized_dict[origin_gene] = origin_gene_dict
+        else: 
+            empty_dict_ids.append(origin_gene)
 
     # sanity check, is are all genes still in the dict, are all scores in range [0,1]?
     for gene_id in sim_score_dict.keys():
-        assert len(sim_score_dict[gene_id]) == len(normalized_dict[gene_id]), f"Missing normalized score for gene pair ({gene_id}[{len(normalized_dict[gene_id])}], {sim_score_dict[gene_id]}[{len(normalized_dict[gene_id])}])"
-        assert max(normalized_dict[gene_id].values()) <= 1 and min(normalized_dict[gene_id].values()) >= 0, f"Probability score for candidate out of range [0,1] for gene {gene_id}: {normalized_dict[gene_id].values()}"
-    print(normalized_dict)
-    quit()
+        if gene_id in normalized_dict:
+            # length of each candidate dict should be old length -1 since we removed self comparisons
+            assert len(sim_score_dict[gene_id]) == len(normalized_dict[gene_id]) + 1, f"Missing normalized score for gene pair ({gene_id}: {normalized_dict[gene_id].keys()}[{len(normalized_dict[gene_id])}], {sim_score_dict[gene_id].keys()}[{len(sim_score_dict[gene_id])}])"
+            assert max(normalized_dict[gene_id].values()) <= (1 + epsilon) and min(normalized_dict[gene_id].values()) >= epsilon, f"Probability score for candidate out of range [0,1] for gene {gene_id}: {normalized_dict[gene_id].values()}"
+
+    log.info(f"Normalized similarity scores between gene candidate with loss of {len(empty_dict_ids)} genes in total, e.g. due to only having self comparisons.")
+    return normalized_dict