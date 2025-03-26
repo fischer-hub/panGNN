210a70624bda433f9493d2455c9ee0867226cf28
Fischer, David <FischerD@rki.de>
2025-03-26 14:58:41 +0100

    fix rbh device issue

--- pangnn.py
+++ pangnn.py
@@ -320,7 +320,8 @@ elif args.train:
         # get metrics on test dataset
         for batch in test_data_loader:
             prediction_bin, prediction_scores, stats = predict_homolog_genes(model, None, batch, binary_th=binary_th, base_labels = dataset.base_labels)
-            brh_refined = reciprocal_best_hits_refined(batch.to_data_list(), dataset.sim_score_dict, prediction_scores)
+            data_lst = batch.cpu().to_data_list()
+            brh_refined = reciprocal_best_hits_refined(data_lst, dataset.sim_score_dict, prediction_scores)
             prediction_bin, prediction_scores, stats = predict_homolog_genes(model, None, batch, binary_th=binary_th, base_labels = dataset.base_labels, refined_base_labels = brh_refined)
             writer.add_pr_curve('PR/test', batch.y.cpu(), torch.sigmoid(prediction_scores))
 
