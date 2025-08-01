767788fd33708d247bddfc007b333807ec9c8616
Fischer, David <FischerD@rki.de>
2025-08-01 15:15:13 +0200

    typo

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -369,7 +369,7 @@ def remove_trivial_cases(sim_score_dict):
         filtered_candidates = { key: val for key, val in target_genes.items() if key.split('_')[0] in non_unique_target_genome_ids }
         if filtered_candidates: filtered_sim_score_dict[source_gene] = filtered_candidates
 
-    log.info(f'Ignoring {len(sim_score_dict) - len(filtered_sim_score_dict)} of {old_len} scores because they were the only candidate in their candidate set.')
+    log.info(f'Ignoring {len(sim_score_dict) - len(filtered_sim_score_dict)} of {len(sim_score_dict)} scores because they were the only candidate in their candidate set.')
     
     return filtered_sim_score_dict
 
